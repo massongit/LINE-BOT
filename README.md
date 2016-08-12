121d06bfdc3a6966b1d096d9b89ff54dfd57b128
massongit <u5t2bu@bma.biglobe.ne.jp>
2016-08-12 18:47:13 +0900

    係り受け解析機能を追加

--- README.md
+++ README.md
@@ -1,25 +1,29 @@
 #名称
-BOTっぽい何か ver.1.0.1
+BOTっぽい何か ver.1.0.2
 
 #作者
 鈴木雅也
 
 #開発言語
-Python 3.5.1
+Python 3.5.2
 
 #使用サービス
-Heroku
+* [Heroku](https://www.heroku.com/)
+* [日本語係り受け解析(Yahoo! JAPAN Webサービス)](http://developer.yahoo.co.jp/webapi/jlp/da/v1/parse.html)
 
 #概要
-Herokuで動かすことを想定したLINE BOTです。
+[Heroku](https://www.heroku.com/)で動かすことを想定したLINE BOTです。
 
 #機能
 * 暴言が来たら、『なんだと(# ﾟДﾟ)』と返す
 * 『{1文字以上の文字列}っぽー』と来たら、『ほっほー(・∀・)』と返す
-* 上記2つに当てはまらない文字列が来たら、オウム返しする
+* 上記2つに当てはまらない文字列が来たら、係り受け解析を行う
 
 #LINE BOT API用CallBack URL
 https://{アプリ名}.herokuapp.com:443/callback
+
+#注意事項
+Yahoo! JAPAN Webサービスへの登録が必要です(http://developer.yahoo.co.jp/start/)。
 
 #デプロイ
 [![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import logging
 import os
 import re
 
+import bs4
 import falcon
 import requests
 
@@ -29,13 +30,11 @@ class CallbackResource:
         """
 
         # 受信データ読み取り用パターン
-        pattern = [
-            re.compile(r'.+っ+ほ'),
-            re.compile(r'[あア][ー〜]*っ*[ほホ]'),
-            re.compile(r'[ばバ][ー〜]*っ*[かカ]'),
-            re.compile(r'(ドジ|どじ)'),
-            re.compile(r'(マヌケ|まぬけ)')
-        ]
+        pattern = [re.compile(r'.+っ+ほ'),
+                   re.compile(r'[あア][ー〜]*っ*[ほホ]'),
+                   re.compile(r'[ばバ][ー〜]*っ*[かカ]'),
+                   re.compile(r'(ドジ|どじ)'),
+                   re.compile(r'(マヌケ|まぬけ)')]
 
         # リクエストのBody
         body = req.stream.read()
@@ -54,29 +53,41 @@ class CallbackResource:
             logger.debug('req_data: {}'.format(req_data))
 
             # 送信データ
-            res_data = {
-                'to': [req_data['content']['from']],
-                'toChannel': 1383378250,
-                'eventType': '138311608800106203'
-            }
+            res_data = {'to': [req_data['content']['from']], 'toChannel': 1383378250, 'eventType': '138311608800106203'}
 
-            # 受信データが文字以外(スタンプ等)である、または、patternで定義されているパターンに当てはまらないならばオウム返し
-            if req_data['content']['text'] is None \
-                    or [p.search(req_data['content']['text']) is not None for p in pattern].count(True) == 0:
-                res_data['content'] = req_data['content']
-            # 受信データがpatternで定義されているパターンに当てはまるとき
-            else:
-                res_data['content'] = {
-                    'contentType': 1,
-                    'toType': 1
-                }
+            if req_data['content']['text']:  # 受信データが文字であるとき
+                res_data['content'] = {'contentType': 1, 'toType': 1}
 
-                # 受信データが暴言ならば、『なんだと(# ﾟДﾟ)』を返す
-                if 0 < [p.search(req_data['content']['text']) is not None for p in pattern[1:]].count(True):
+                # patternで定義されている各パターンに当てはまるかどうか
+                pr = [p.search(req_data['content']['text']) is not None for p in pattern]
+
+                if 0 < pr[1:].count(True):  # 受信データが暴言ならば、『なんだと(# ﾟДﾟ)』を返す
                     res_data['content']['text'] = 'なんだと(# ﾟДﾟ)'
-                # 受信データが暴言でなく、かつ、正規表現『.+っほー.*』に当てはまるパターンならば、『ほっほー(・∀・)』を返す
-                else:
+                elif pr[0]:  # 受信データが暴言でなく、かつ、正規表現『.+っほー.*』に当てはまるパターンならば、『ほっほー(・∀・)』を返す
                     res_data['content']['text'] = 'ほっほー(・∀・)'
+                else:  # patternで定義されているパターンに当てはまらないならば、係り受け解析を行い、結果を返す
+                    # 係り受け解析結果
+                    da = requests.post('http://jlp.yahooapis.jp/DAService/V1/parse',
+                                       {'appid': os.environ['YAHOO_JAPAN_WEB_SERVICE_APPLICATION_ID'],
+                                        'sentence': req_data['content']['text']})
+
+                    # 結果
+                    r = list()
+                    r.append('\t'.join(['文節番号', '修飾先の文節番号', '表記', 'よみがな', '基本形表記', '品詞', '形態素情報']))
+
+                    for c in bs4.BeautifulSoup(da.content, "html.parser").find_all('chunk'):
+                        for m in c.find_all('morphem'):
+                            r.append('\t'.join([t.text.replace('\n', '{改行}') for t in [c.find('id'),
+                                                                                       c.find('dependency'),
+                                                                                       m.find('surface'),
+                                                                                       m.find('reading'),
+                                                                                       m.find('baseform'),
+                                                                                       m.find('pos'),
+                                                                                       m.find('feature')]]))
+
+                    res_data['content']['text'] = '\n'.join(r)
+            else:  # 受信データが文字でないならば、オウム返しする
+                res_data['content'] = req_data['content']
 
             # 送信データを文字列化
             res_data = json.dumps(res_data)
@@ -86,13 +97,11 @@ class CallbackResource:
             # レスポンスを送信
             res = requests.post('https://trialbot-api.line.me/v1/events',
                                 data=res_data,
-                                headers={
-                                    'Content-Type': 'application/json; charset=UTF-8',
-                                    'X-Line-ChannelID': os.environ['LINE_CHANNEL_ID'],
-                                    'X-Line-ChannelSecret': os.environ['LINE_CHANNEL_SECRET'],
-                                    'X-Line-Trusted-User-With-ACL': os.environ['LINE_CHANNEL_MID']
-                                },
-                                proxies={'https': os.environ.get('FIXIE_URL', '')})
+                                headers={'Content-Type': 'application/json; charset=UTF-8',
+                                         'X-Line-ChannelID': os.environ['LINE_CHANNEL_ID'],
+                                         'X-Line-ChannelSecret': os.environ['LINE_CHANNEL_SECRET'],
+                                         'X-Line-Trusted-User-With-ACL': os.environ['LINE_CHANNEL_MID']},
+                                proxies={'https': os.environ['FIXIE_URL']})
 
             logger.debug('res: {} {}'.format(res.status_code, res.reason))
 
